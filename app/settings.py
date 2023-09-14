3a06706d34bac5cb8849628c153c61a8f8703b56
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2023-09-14 13:12:15 +0200

    Removed settings as a class

--- app/settings.py
+++ app/settings.py
@@ -1,29 +1,25 @@
 from os import environ
 
 
-class Settings:
-    """
-    Class to handle the settings.
-    """
-    #################################################################
-    # GENERAL
-    #################################################################
-    EXPIRE_DAYS = int(environ.get('EXPIRE_DAYS', 30))
+#################################################################
+# GENERAL
+#################################################################
+EXPIRE_DAYS = int(environ.get('EXPIRE_DAYS', 30))
 
-    #################################################################
-    # AZURE ACCESS SPECIFIC
-    #################################################################
-    AZURE_CLIENT_ID = environ.get('AZURE_CLIENT_ID')
-    AZURE_CLIENT_SECRET = environ.get('AZURE_CLIENT_SECRET')
-    AZURE_TENANT_ID = environ.get('AZURE_TENANT_ID')
+#################################################################
+# AZURE ACCESS SPECIFIC
+#################################################################
+AZURE_CLIENT_ID = environ.get('AZURE_CLIENT_ID')
+AZURE_CLIENT_SECRET = environ.get('AZURE_CLIENT_SECRET')
+AZURE_TENANT_ID = environ.get('AZURE_TENANT_ID')
 
-    #################################################################
-    # EMAIL SPECIFIC
-    #################################################################
-    USE_MAILJET_LIB = bool(environ.get('USE_MAILJET_LIB', False))
-    EMAIL_HOST = environ.get('EMAIL_HOST')
-    EMAIL_FROM = environ.get('EMAIL_FROM')
-    EMAIL_FROM_NAME = environ.get('EMAIL_FROM_NAME')
-    EMAIL_TO = environ.get('EMAIL_TO')
-    EMAIL_USERNAME = environ.get('EMAIL_USERNAME')
-    EMAIL_PASSWORD = environ.get('EMAIL_PASSWORD')
+#################################################################
+# EMAIL SPECIFIC
+#################################################################
+USE_MAILJET_LIB = bool(environ.get('USE_MAILJET_LIB', False))
+EMAIL_HOST = environ.get('EMAIL_HOST')
+EMAIL_FROM = environ.get('EMAIL_FROM')
+EMAIL_FROM_NAME = environ.get('EMAIL_FROM_NAME')
+EMAIL_TO = environ.get('EMAIL_TO')
+EMAIL_USERNAME = environ.get('EMAIL_USERNAME')
+EMAIL_PASSWORD = environ.get('EMAIL_PASSWORD')
